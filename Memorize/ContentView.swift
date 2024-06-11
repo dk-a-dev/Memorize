7e3b08fc32cb5bbd0f8deaf7fae9caa7a0beac23
Dev Keshwani <dev.keshwani345@gmail.com>
2024-06-11 20:34:29 +0530

    feat: added buttons for dynamic addition and removal

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    let emojies=["🥳","🥰","😘","🤩"]
+    let emojies=["🥳","🥰","😘","🤩","😇"]
+    @State var cardCount=4
+    func checkCount(offset:Int) -> Array<Bool> {
+        if(offset>0 && cardCount>=emojies.count){
+            return [true,false]
+        }
+        if(offset<0 && cardCount<=1){
+            return [false,true]
+        }
+        return [false,false]
+    }
     var body: some View {
-        HStack{
-            ForEach(0..<emojies.count,id: \.self){ index in
-                CardView(content: emojies[index])
+        VStack{
+            HStack{
+                ForEach(0..<cardCount,id: \.self){ index in
+                    CardView(content: emojies[index])
+                }
+            }.padding().foregroundColor(.red)
+            HStack{
+                Button(action: {
+                    cardCount+=1
+                }) {
+                    Image(systemName: "rectangle.stack.fill.badge.plus").font(.largeTitle)
+                }.disabled(checkCount(offset: +1)[0])
+                Spacer()
+                Button(action: {
+                    cardCount-=1
+                }) {
+                    Image(systemName: "rectangle.stack.fill.badge.minus").font(.largeTitle)
+                }.disabled(checkCount(offset: -1)[1])
             }
-        }.padding().foregroundColor(.red)
+        }.padding()
     }
     
 }
@@ -24,21 +49,20 @@ struct CardView:View {
     let content: String
     @State var isFaceUp = false
     var body: some View{
-        ZStack{
-            let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 5)
-                Text(content).font(.largeTitle)
+            ZStack{
+                let base = RoundedRectangle(cornerRadius: 12)
+                if isFaceUp{
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 5)
+                    Text(content).font(.largeTitle)
+                }
+                else{
+                    base.fill()
+                }
+            }.onTapGesture{
+                isFaceUp.toggle()
             }
-            else{
-                base.fill()
-            }
-        }.onTapGesture{
-            isFaceUp.toggle()
-        }
     }
-    
 }
 
 #Preview {

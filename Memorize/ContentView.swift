83139bd0d366da9f98f4dffd1345b8523dc8631b
Dev Keshwani <dev.keshwani345@gmail.com>
2024-06-13 23:49:36 +0530

    feat: chnages  as per a1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,44 +8,67 @@
 import SwiftUI
 import SwiftData
 
+
+
 struct ContentView: View {
-    let emojies=["🥳","🥰","😘","🤩","😇","🤓","😎","🎃","🙂","🤣","🤪"]
-    @State var cardCount=4
-    func checkCount(offset:Int) -> Array<Bool> {
-        if(offset>0 && cardCount>=emojies.count){
-            return [true,false]
+    @State var emojies:Array<String>=[]
+    var halloween=["👻","🎃","💀","��","👺","☠️"]
+    var vechiles=["🚗","🚙","🚋","🚎","🚝","🚁","🚲","🛩️","🏍️","🚒"]
+    var countries=["🇬🇧","🇦🇷","🇧🇩","🇮🇳","🇦🇺"]
+
+    func getThemedCards(id:Int){
+        emojies.removeAll()
+        switch id {
+        case 1:
+            emojies = halloween.shuffled() + emojies
+        case 2:
+            emojies = countries.shuffled() + emojies
+        case 3:
+            emojies = vechiles.shuffled() + emojies
+        default:
+            emojies = []
         }
-        if(offset<0 && cardCount<=1){
-            return [false,true]
-        }
-        return [false,false]
     }
     var body: some View {
+        Text("Memorize!").padding().fontWeight(.bold).font(.largeTitle)
         VStack{
-            ScrollView{
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
-                        ForEach(0..<cardCount,id: \.self){ index in
-                            CardView(content: emojies[index]).aspectRatio(1,contentMode: .fit)
-                        }
-                    }.foregroundColor(.pink)
-                }
+            LazyVGrid(columns: [GridItem(),GridItem(),GridItem(),GridItem()]){
+                    ForEach(0..<emojies.count,id: \.self){ index in
+                        CardView(content: emojies[index]).aspectRatio(2/3,contentMode: .fit)
+                    }
+                }.foregroundColor(.pink)
             Spacer()
-            HStack{
-                Button(action: {
-                    cardCount-=1
-                }) {
-                    Image(systemName: "rectangle.stack.fill.badge.minus").font(.largeTitle)
-                }.disabled(checkCount(offset: -1)[1])
-                Spacer()
-                Button(action: {
-                    cardCount+=1
-                }) {
-                    Image(systemName: "rectangle.stack.fill.badge.plus").font(.largeTitle)
-                }.disabled(checkCount(offset: +1)[0])
-            }
         }.padding()
+        Spacer()
+        HStack(content: {
+            Button {
+                getThemedCards(id: 1)
+                    } label: {
+                        ButtonView(icon: "figure.fall.circle", title: "halloween")
+                    }
+            Button {
+                getThemedCards(id: 2)
+                    } label: {
+                        ButtonView(icon: "globe", title: "countries")
+                    }
+            Button {
+                getThemedCards(id: 3)
+                    } label: {
+                        ButtonView(icon: "car", title: "vechiles")
+                    }
+        })
     }
-    
+}
+
+struct ButtonView:View {
+    let icon:String
+    let title:String
+    var body: some View{
+        VStack(content: {
+            Image(systemName: icon).font(.largeTitle)
+            Text(title).font(.headline)
+        })
+    }
 }
 
 struct CardView:View {
